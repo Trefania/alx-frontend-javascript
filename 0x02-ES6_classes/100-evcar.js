b0f5cb5f00a7133e8f486f89ea9ca32f2567cc8e
trefania <tarirotrefania@gmail.com>
2023-04-21 00:44:02 +0200

    hoisting

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const clone = super.cloneCar();
+    return new Car(clone._brand, clone._motor, clone._color);
+  }
+}
